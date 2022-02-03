59a5165751f15eb0fc674fb5d1225c77b7f7bd59
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-03 10:27:33 +0200

    corrected the functionality and fixed linter errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,28 @@
 import '../style.css';
 
-import {refreshData, addPlayer, createGame } from './interact-api'
+import { refreshData, addPlayer } from './interact-api.js';
+
 const leaderboard = document.querySelector('.score-list');
-const refreshBtn =document.getElementById('refresh');
+const refreshBtn = document.getElementById('refresh');
 const form = document.querySelector('form');
 const names = document.getElementById('name');
 const scores = document.getElementById('score');
 let players;
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    refreshData(players,leaderboard);
-} );
-
-refreshBtn.addEventListener('click', ()=>{
-    refreshData(players, leaderboard);
+window.addEventListener('DOMContentLoaded', () => {
+  refreshData(players, leaderboard);
 });
 
-form.addEventListener('submit', async (e) =>{
-    const obj = {
-        user: document.getElementById('name').value,
-        score: document.getElementById('score').value
-        
-    }
-    e.preventDefault();
-     await addPlayer(obj);
-    // refreshData(players, leaderboard);
-    form.reset();
+refreshBtn.addEventListener('click', () => {
+  refreshData(players, leaderboard);
+});
+
+form.addEventListener('submit', async (e) => {
+  const obj = {
+    user: names.value,
+    score: scores.value,
+  };
+  e.preventDefault();
+  await addPlayer(obj);
+  form.reset();
 });
--- src/js/interact-api.js
+++ src/js/interact-api.js
@@ -1,40 +1,38 @@
-import displayData from "./displayData";
+import displayData from './displayData.js';
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/A9GgGr2ZTgKdQScdJRGi/scores/'
-// game code is A9GgGr2ZTgKdQScdJRGi  h6iR6MU3r9EJmryOWw9J
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/A9GgGr2ZTgKdQScdJRGi/scores/';
+
 export const createGame = () => {
-    const game = {
-        name: 'NBA Live',
-    };
-    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-        method: 'POST',
-        body: JSON.stringify(game),
-        headers: {
-            'Content-type': 'application/json; charset= UTF-8', 
-        },
-    })
-
+  const game = {
+    name: 'NBA Live',
+  };
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify(game),
+    headers: {
+      'Content-type': 'application/json; charset= UTF-8',
+    },
+  });
 };
 
 export const refreshData = (players, leaderboard) => {
-
-    fetch(url)
-    .then((response)=> response.json())
-    .then((json)=> {
-        players = json.result;
-        displayData(players, leaderboard)
-    })
-}
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      players = json.result;
+      displayData(players, leaderboard);
+    });
+};
 
 export const addPlayer = async (data) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json; UTF-8',
-        },
-    });
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
 
-    const {result} = await response.json();
-    return result;
+  const { result } = await response.json();
+  return result;
 };
